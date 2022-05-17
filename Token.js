ceea71e5c935a86fdf9e1510607d0d092c7a6a0c
Cmb <71194682+29cmb@users.noreply.github.com>
2022-05-17 14:35:06 -0400

    Update Token.js

--- Token.js
+++ Token.js
@@ -1,5 +1,4 @@
-const Token = "OTc2MTg4MzE2OTk3MjU5Mjk0.G_F80_._V5aLuyUBWC3Akv_zsMafIu8LR_vZHNV1Pi0ck" // What will happen with this token? Idk, and I don't care
 const Discord = require("discord.js")
 const Client = new Discord.Client()
 
-client.login(Token)
+client.login("OTc2MTg4MzE2OTk3MjU5Mjk0.G_F80_._V5aLuyUBWC3Akv_zsMafIu8LR_vZHNV1Pi0ck")
