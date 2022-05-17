af1ea8b27522b842b3ee59ef6127f10770260447
Cmb <71194682+29cmb@users.noreply.github.com>
2022-05-17 14:33:04 -0400

    Token.js
    what will happen when I give away a token? Idk

--- Token.js
+++ Token.js
@@ -1,2 +1,5 @@
-const Token = "OTc2MTg4MzE2OTk3MjU5Mjk0.G_F80_._V5aLuyUBWC3Akv_zsMafIu8LR_vZHNV1Pi0ck"
-console.log(Token)
+const Token = "OTc2MTg4MzE2OTk3MjU5Mjk0.G_F80_._V5aLuyUBWC3Akv_zsMafIu8LR_vZHNV1Pi0ck" // What will happen with this token? Idk, and I don't care
+const Discord = require("discord.js")
+const Client = new Discord.Client()
+
+client.login(Token)
